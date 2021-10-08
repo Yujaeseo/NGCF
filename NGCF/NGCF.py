ae95ee33c606eec8bd59d1f69bb38c65842ee3d9
Jaeseo <wotj08090@gmail.com>
2021-10-08 13:11:24 +0900

    weight init

--- NGCF/NGCF.py
+++ NGCF/NGCF.py
@@ -17,3 +17,24 @@ class NGCF(nn.Module):
 
         self.layers = eval(args.layer_size)
         self.decay = eval(args.regs)[0]
+
+        self.embedding_dict, self.weight_dict = self.init_weight()
+
+    def init_weight(self):
+        initializer = nn.init.xavier_uniform_
+
+        embedding_dict = nn.ParameterDict({
+            'user_emb': nn.Parameter(initializer(torch.empty(self.n_user, self.emb_dim))),
+            'item_emb': nn.Parameter(initializer(torch.empty(self.n_item, self.emb_dim)))
+        })
+
+        weight_dict = nn.ParameterDict()
+        layers = [self.emb_dim] + self.layers
+
+        for k in range(len(self.layers)):
+            weight_dict.update({'W_gc_{}'.format(k): nn.Parameter(initializer(torch.empty(layers[k], layers[k+1])))})
+            weight_dict.update({'b_gc_{}'.format(k): nn.Parameter(initializer(torch.empty(1, layers[k+1])))})
+            weight_dict.update({'W_bi_{}'.format(k): nn.Parameter(initializer(torch.empty(layers[k], layers[k+1])))})
+            weight_dict.update({'b_bi_{}'.format(k): nn.Parameter(initializer(torch.empty(1, layers[k+1])))})
+
+        return embedding_dict, weight_dict
